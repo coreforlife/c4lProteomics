46b3242948281bef774537cc3468a12eb1c6ea36
Christian Panse <cp@fgcz.ethz.ch>
2019-01-23 16:19:18 +0100

    added now minus 36h feature

--- fgcz_robot/fgcz_robot__sync_qcloud2.bash
+++ fgcz_robot/fgcz_robot__sync_qcloud2.bash
@@ -26,7 +26,7 @@ flock -n 200 || exit 1
 declare -A labsysid=(["QEXACTIVEHF_2"]="41102513-a4c3-4e23-9a05-3151620a7c8c"
   ["QEXACTIVEHFX_1"]="fef21e50-b532-4c0e-9669-852972e28366")
 
-input=/srv/www/htdocs/Data2San/sync_LOGS/pfiles.txt
+INPUT=/srv/www/htdocs/Data2San/sync_LOGS/pfiles.txt
 
 
 #######################################
@@ -50,20 +50,29 @@ function compose_lftp_command () {
 }
 
 
-# Main
-awk -F";" '$NF~/autoQC(01|02|4L).*.raw$/ && $2>1547758810{print}' $input \
-  | egrep "QEXACTIVEHF_2|QEXACTIVEHFX_1" \
-  | egrep "autoQC4L|autoQC01" \
-  | while read i;
-  do 
-    filepath=`echo $i | cut -d';' -f4`
-    filename=`basename $filepath .raw`
-    instrument=`echo $filepath | cut -d'/' -f3`
-    qctype=`echo $filename | sed 's/.*auto\(QC..\).*/\1/' | sed 's/QC4L/QC03/g'`
-    md5=`echo $i | cut -d';' -f1`
+function main () {
+  set -x
+  local INSTRUMENTPATTERN=`echo ${!labsysid[@]} | tr " " "|"`
+  local QCPATTERN="(autoQC4L|autoQC01).*\.raw$"
+  local now=`date  "+%s"`
+
+  set +x
+
+  awk -F";" -v now=$now '$2 > (now - 60 * 60 * 36){print}' $INPUT \
+    | egrep "(${INSTRUMENTPATTERN}).*${QCPATTERN}" \
+    | while read i;
+    do 
+      filepath=`echo $i | cut -d';' -f4`
+      filename=`basename $filepath .raw`
+      instrument=`echo $filepath | cut -d'/' -f3`
+      qctype=`echo $filename | sed 's/.*auto\(QC..\).*/\1/' | sed 's/QC4L/QC03/g'`
+      md5=`echo $i | cut -d';' -f1`
     
-    sleep 1
-    compose_lftp_command '/srv/www/htdocs/' $filepath $filename $instrument $qctype $md5
-  done
+      sleep 1
+      compose_lftp_command '/srv/www/htdocs/' $filepath $filename $instrument $qctype $md5
+    done
+}
+
+main
 
 exit 0
