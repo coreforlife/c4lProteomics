a73c31422a60021bdadaff4156deeb8c56b7ef67
Christian Panse <cpanse@users.noreply.github.com>
2019-01-24 15:49:49 +0100

    Update fgcz_robot__sync_qcloud2.bash
    Added documentation

--- fgcz_robot/fgcz_robot__sync_qcloud2.bash
+++ fgcz_robot/fgcz_robot__sync_qcloud2.bash
@@ -22,12 +22,20 @@ lock="/tmp/${scriptname}"
 exec 200>$lock
 flock -n 200 || exit 1
 
+#######################################
+# instrument to labsysid linkage
 # given by <roger.olivella@crg.eu>
+#######################################
 declare -A labsysid=(["QEXACTIVEHF_2"]="41102513-a4c3-4e23-9a05-3151620a7c8c"
   ["QEXACTIVEHFX_1"]="fef21e50-b532-4c0e-9669-852972e28366")
 
+#######################################
+# Example:
+# md5;time;size;filepath
+# f336ff5cf2638759bddf1e99018f2bb0;1548307809;77991976;p3000/Proteomics/QEXACTIVEHFX_1/lkunz_20190123_HeLa_phospho_div_tests/20190123_11_autoQC01.raw
+# 0aa572aa91be0cca43f41515b2903231;1548340209;73099850;p3000/Proteomics/QEXACTIVEHFX_1/lkunz_20190123_HeLa_phospho_div_tests/20190123_18_autoQC01.raw
+#######################################
 INPUT=/srv/www/htdocs/Data2San/sync_LOGS/pfiles.txt
-
 
 #######################################
 # Compose one lftp command for each not transfered raw file 
@@ -58,7 +66,7 @@ function main () {
 
   set +x
 
-  awk -F";" -v now=$now '$2 > (now - 60 * 60 * 36){print}' $INPUT \
+  awk -F';' -v now=$now '$2 > (now - 60 * 60 * 36){print}' $INPUT \
     | egrep "(${INSTRUMENTPATTERN}).*${QCPATTERN}" \
     | while read i;
     do 
