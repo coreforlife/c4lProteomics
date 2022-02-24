f198f2be8a2ee1219f0b51209ebe0aeb1f20d892
Christian Panse <cp@fgcz.ethz.ch>
2022-02-24 15:16:38 +0100

    Check all transfer_log* files
    - for whatsoever reasons the transfer_log is renamed to transfer_log.old when the file size is higher than 1MB
    - some bash script cosmentics
    - consider only files recorded within the last 24 hours

--- fgcz_robot/fgcz_robot__sync_qcloud2.bash
+++ fgcz_robot/fgcz_robot__sync_qcloud2.bash
@@ -43,7 +43,8 @@ declare -A labsysid=(["QEXACTIVEHF_2"]="41102513-a4c3-4e23-9a05-3151620a7c8c"
 # f336ff5cf2638759bddf1e99018f2bb0;1548307809;77991976;p3000/Proteomics/QEXACTIVEHFX_1/lkunz_20190123_HeLa_phospho_div_tests/20190123_11_autoQC01.raw
 # 0aa572aa91be0cca43f41515b2903231;1548340209;73099850;p3000/Proteomics/QEXACTIVEHFX_1/lkunz_20190123_HeLa_phospho_div_tests/20190123_18_autoQC01.raw
 #######################################
-INPUT=/srv/www/htdocs/Data2San/sync_LOGS/pfiles.txt
+INPUT="/srv/www/htdocs/Data2San/sync_LOGS/pfiles.txt"
+[[ -f ${INPUT} ]] || { echo "not $INPUT file."; exit 1; }
 
 #######################################
 # Compose one lftp command for each not transfered raw file 
@@ -56,7 +57,7 @@ INPUT=/srv/www/htdocs/Data2San/sync_LOGS/pfiles.txt
 #   stdout
 #######################################
 function compose_lftp_command () {
-  grep "`basename $2`\|$6" ~/.lftp/transfer_log > /dev/null
+  grep "$(basename $2)\|$6" ~cpanse/.lftp/transfer_log* > /dev/null
   if [ $? -eq 0 ]
     then 
       echo "# file '$2' ALREADY COPIED"
@@ -68,23 +69,23 @@ function compose_lftp_command () {
 
 
 function main () {
-  set -x
+  #set -x
   local INSTRUMENTPATTERN=`echo ${!labsysid[@]} | tr " " "|"`
   # local QCPATTERN="(autoQC01).*\.raw$"
   local QCPATTERN="(autoQC4L|autoQC01).*\.raw$"
-  local now=`date  "+%s"`
+  local now=$(date  "+%s")
 
-  set +x
+  #set +x
 
-  awk -F';' -v now=$now '$2 > (now - 60 * 60 * 24 * 10){print}' $INPUT \
+  awk -F';' -v now=$now '$2 > (now - 60 * 60 * 24){print}' $INPUT \
     | egrep "(${INSTRUMENTPATTERN}).*${QCPATTERN}" \
     | while read i;
     do 
-      filepath=`echo $i | cut -d';' -f4`
-      filename=`basename $filepath .raw`
-      instrument=`echo $filepath | cut -d'/' -f3`
-      qctype=`echo $filename | sed 's/.*auto\(QC..\).*/\1/' | sed 's/QC4L/QC03/g'`
-      md5=`echo $i | cut -d';' -f1`
+      filepath=$(echo $i | cut -d';' -f4)
+      filename=$(basename $filepath .raw)
+      instrument=$(echo $filepath | cut -d'/' -f3)
+      qctype=$(echo $filename | sed 's/.*auto\(QC..\).*/\1/' | sed 's/QC4L/QC03/g')
+      md5=$(echo $i | cut -d';' -f1)
     
       #sleep 1
       compose_lftp_command '/srv/www/htdocs/' $filepath $filename $instrument $qctype $md5
